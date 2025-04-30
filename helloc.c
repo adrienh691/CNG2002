80f25b155187ab6411e99427cf2f93f359d514bb
adrienh691 <adrianh691@lpsk12.org>
2025-04-30 10:32:10 -0600

    Add files via upload

---
+++ helloc.c
@@ -0,0 +1,9 @@
+// uses stdio.h file library
+#include <stdio.h>
+
+// main function. prints hello world
+int main() {
+	printf("Hello world!");
+	return 0;
+}
+// return0 ends function
---
+++ hellocpp.cpp
@@ -0,0 +1,9 @@
+// includes given file library to pull from, using standard naming
+#include <iostream>
+using namespace std;
+
+// main function, prints hello world. return0 ends function.
+int main() {
+	cout << "Hello world!";
+	return 0;
+}
